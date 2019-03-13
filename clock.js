4c6d936bdf4dd94e79b11a9bf74b86ccc8f2164b
Blonded <lmartinson20@gmail.com>
2019-03-12 18:18:36 -0700

    cleanup

--- clock.js
+++ clock.js
@@ -36,6 +36,7 @@ digClock();
 // To run the function every second thereafter.
 setInterval(digClock, 1000);
 
+
 // Current Date
 
 var today = new Date();
@@ -43,13 +44,13 @@ var dd = today.getDate();
 var mm =  today.getMonth()+1;
 var yyyy = today.getFullYear();
 
-if (dd < 10) {
-    dd = '0' + dd;
-}
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
 
-if (mm < 10){
-    mm = '0' + mm;
-}
+    if (mm < 10){
+        mm = '0' + mm;
+    }
 
 today = mm + '/' + dd + '/' + yyyy;
 document.write(today);
