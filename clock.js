0fc322d5b8489981a34423157324bf8e406990ed
Blonded <lmartinson20@gmail.com>
2019-03-12 17:37:03 -0700

    new code testing

--- clock.js
+++ clock.js
@@ -1,21 +1,9 @@
 
 // Setting variables for clock operations
-function showTime(){
-    var date = new Date();
-    // Setting Hours
-    var h = date.getHours(); // 0-23hrs
-    // Setting Minutes
-    var m = date.getMinutes(); // 0-59 min
-    // Setting Seconds
-    var s = date.getSeconds(); // 0-59 sec
+    var time = new Date();
 
-// Generate where the javascript will take place, in this case it will be in the clockDisplay Div.
+console.log(time.getHours());
+console.log(time.getMinutes());
+console.log(time.getSeconds());
 
-//will not work in firefox
-document.getElementById("#clockDisplay").innerHTML = time;
-//wont work in internet explorer
-document.getElementById("clockDisplay").textContent = time;
-
-}
-
-showTime();
+// Check to make sure you are receiving the correct timing.
